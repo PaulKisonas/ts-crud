d1f6ae200924af656c393e0ec9dc34455a056988
Pkishonas <pauliuskisonas94@gmail.com>
2023-01-29 18:57:27 +0200

    Feat(Cao 12 TS): fixed app

--- src/components/app.ts
+++ src/components/app.ts
@@ -1,7 +1,9 @@
+import Table from "./table";
 import cars from "../data/cars";
 import brands from "../data/brands";
 import models from "../data/models";
 import CarsCollection from '../helpers/cars-collection';
+import stringifyProps from "../helpers/stingify-object";
 
 class App {
   private htmlElement: HTMLElement;
@@ -18,10 +20,22 @@ class App {
   }
 
   initialize = (): void => {
+    const carTable = new Table({
+      title: 'Visi automobiliai',
+      columns: {
+        id: 'Id',
+        brand: 'Markė',
+        model: 'Modelis',
+        price: 'Kaina',
+        year: 'Metai',
+      },
+      rowsData: this.carsCollection.all.map(stringifyProps),
+    });;
 
     const container = document.createElement('div');
     container.className = 'container my-5';
-    
+    container.appendChild(carTable.htmlElement);
+
     this.htmlElement.append(container);
   };
 }
